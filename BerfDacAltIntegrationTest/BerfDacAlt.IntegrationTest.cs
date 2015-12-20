c6b98c6b5c9c1d5e74a3f065e3183c1a1d6c36d2
johnkennerley <john.kennerley@gmail.com>
2015-12-20 20:07:38 +0000

    ef and installed xunit inot itg test, so can see test in test explorer.

--- BerfDacAltIntegrationTest/BerfDacAlt.IntegrationTest.cs
+++ BerfDacAltIntegrationTest/BerfDacAlt.IntegrationTest.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
-using Xunit;
-
-// namespace BerfDacAltIntegrationTest
-// {
-//     public class BerfDacAltIntegrationTest
-//     {
-//            [Fact]
-//            public void asb()
-//            {
-//                Assert.Equal(1, 1);
-//            }
-//     }
-// }
-
-
+namespace BerfDacAltIntegrationTest
+{
+    public class BerfDacAltIntegrationTest
+    {
+        [Fact]
+        public void asb_2()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
