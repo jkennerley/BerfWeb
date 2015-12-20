51995d59fbb694ca7c2804c54fa54f2a722624b7
johnkennerley <john.kennerley@gmail.com>
2015-12-20 19:53:51 +0000

    small tidy

--- BerfDacAltIntegrationTest/BerfDacAlt.IntegrationTest.cs
+++ BerfDacAltIntegrationTest/BerfDacAlt.IntegrationTest.cs
@@ -10,29 +10,12 @@ using Xunit;
 // {
 //     public class BerfDacAltIntegrationTest
 //     {
+//            [Fact]
+//            public void asb()
+//            {
+//                Assert.Equal(1, 1);
+//            }
 //     }
 // }
 
 
-namespace MyFirstUWPTests
-{
-    public class UnitTest
-    {
-        [Fact]
-        public void PassingTest()
-        {
-            Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
-    }
-}
